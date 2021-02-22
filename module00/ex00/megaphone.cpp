9ebf65e6e44087de26ab4abf84402289ec6b4954
Jeanne GONFROY <jgonfroy@e2r3p3.42.fr>
2021-02-22 21:58:32 +0100

    etrange... tres etrange

--- module00/ex00/megaphone.cpp
+++ module00/ex00/megaphone.cpp
@@ -1,5 +1,32 @@
+#include <iostream>
+
 int main(int ac, char **av)
 {
-	if (ac == 1)
+	int	i;
+	int	j;
+	char c;
 
+	i = 1;
+	j = 0;
+	if (ac == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	while (av[i] != NULL)
+	{
+		while (av[i][j] != '\0')
+		{
+		//	if (av[i][j] > 96 && av[i][j] < 123)
+		//		c = av[i][j] - 32;
+		//	else
+				c = av[i][j];
+			std::cout << toupper(c);
+			j++;
+		}
+	
+	printf("%s\n", av[i]);
+	i++;
+	}
+	std::cout << std::endl;
 }
