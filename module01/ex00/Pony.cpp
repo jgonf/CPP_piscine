44fd4ea4a23cdd9170cae566cf532f66cb8ad0a1
john doe <v@e3r1p10.42.fr>
2021-02-26 16:07:57 +0000

    premiere pierre

---
+++ module01/ex00/Pony.cpp
@@ -0,0 +1,14 @@
+#ifndef PONY_CPP
+#define PONY_CPP
+
+class pony {
+	private:
+		std::string	_name;
+		int		_age;
+		int		_nb_carries;			
+
+	public:
+		pony(std::string p_name, int p_age, int p_carries )
+};
+
+#endif
