b57b371c25998b82c8ac8b0ef6b22f489759237b
user42 <jeanne.gonfroy@protonmail.com>
2021-03-12 17:44:32 +0100

    get the address but not the content

--- module06/ex01/main.cpp
+++ module06/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jgonfroy <jgonfroy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/12 10:50:51 by jgonfroy          #+#    #+#             */
-/*   Updated: 2021/03/12 15:28:54 by jgonfroy         ###   ########.fr       */
+/*   Updated: 2021/03/12 17:43:34 by jgonfroy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,56 +14,47 @@
 
 void	*serialize(void)
 {
-	int *i = new int;
-	std::string *s1 = new std::string[10];
-	std::string *s2 = new std::string[10];
+	int i;
+	std::string s1;
+	std::string s2;
 	std::stringstream ss;
 	char *ret = new char[sizeof(std::string) * 2 + sizeof(int)]();
 
-	*i = rand() % 1000;
+	i = rand() % 1000;
 	for (int j = 0; j < 10; j++)
 	{
-		s1[j] = rand() % 90 + 49;
-		s2[j] = rand() % 90 + 49;
+		s1 += rand() % 90 + 49;
+		s2 += rand() % 90 + 49;
 	}
-	ss << s1 << i << s2;
+	ss << &s1 << i << &s2;
+	std::cout << s1 << std::endl;
 	strcpy(ret, ss.str().c_str());
+	std::cout << ret << std::endl;
 	return (ret);
 }
 
 data	*deserialize(void *raw)
 {
 	data *res = new data();
-	char * str = reinterpret_cast<char*> (raw);
-	
+//	char * str = reinterpret_cast<char*> (raw);
+	res->s1 = std::string(reinterpret_cast<char *>(raw), sizeof(std::string*) + 1);
 
-//	std::string tmp(str);
-//	tmp = tmp.substr(0, sizeof(std::string*) + 1);
-	std::cout << str << std::endl;
+	char tmp[8];
+	strcpy(tmp, res->s1.c_str());
 	std::cout << tmp << std::endl;
-
-//	char const *str1 = tmp.c_str();	
-//	std::cout << str1 << std::endl;
-//	void *ptr = reinterpret_cast<void *>(str1);
-//	std::cout << ptr << std::endl;
-
-
-//	res->s1 = reinterpret_cast<std::string> (str);
-//	char test = *reinterpret_cast<char *> (raw);
-
-//	std::cout << res->s1 << std::endl;
-//	std::cout << str << std::endl;
-
+//	void *ptr = reinterpret_cast<void *> (tmp);
+	std::cout << *tmp << std::endl;
+//	std::cout << *(res->s1) << std::endl;
 	return res;
 }
 
 int main(void)
 {
 	void *ptr;
-	//data *data;
+	data *data;
 
 	srand(time(NULL));
 	ptr = serialize();
-	deserialize(ptr);
+	data = deserialize(ptr);
 //	delete data;
 }
